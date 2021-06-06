a472f520e2a084e6300114f0f53368bbae685e4b
SuHyun Kyung <ase3238@gmail.com>
2021-06-06 18:05:53 +0900

    feat: session storage에서 userInfo 가져오기 및 보여주기

--- src/main/java/com/simon/test/springboot/web/IndexController.java
+++ src/main/java/com/simon/test/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.simon.test.springboot.web;
 
+import com.simon.test.springboot.config.auth.dto.SessionUser;
 import com.simon.test.springboot.service.posts.PostsService;
 import com.simon.test.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")    // 일종의 router
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc()); // template 변수 매핑
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index"; // 해당하는 이름의 mustache file을 반환해줌.
     }
 
